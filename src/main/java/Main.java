1eaccab448ac4c132fd0b870189edc0fe915ceed
Andrey Nachevny <nochergt@gmail.com>
2023-09-25 19:31:42 +0300

    add test

---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            try {
+                new MyException();
+            } catch (Throwable e) {
+                System.out.println("e = " + e);
+                if (e.getCause() instanceof MyException) {
+                    MyException ex = (MyException) e.getCause();
+                    System.out.println("e instanceof " + MyException.class.getName() + ", s = " + ex.get());
+                }
+            }
+        }
+    }
+}
+
+class MyException extends RuntimeException {
+    private String s;
+
+    static {
+        init();
+    }
+
+    public MyException() {
+        s = "Hello";
+    }
+
+    private static void init() {
+        throw new MyException();
+    }
+
+    public String get() {
+        return s;
+    }
+}
+
